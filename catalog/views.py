17d544820f69a4316a32d1fd805809a9321c71e2
grbcas <grbcas@gmail.com>
2023-08-18 20:35:11 +0300

    add form POST

--- catalog/views.py
+++ catalog/views.py
@@ -9,7 +9,7 @@ def index(request):
 def contacts(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        email = request.POST.get('email')
+        phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({email}): {message}')
+        print(f'{name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
