8de40820177077372a5e5321a6d2f6d45deaacaf
grbcas <grbcas@gmail.com>
2023-09-13 21:26:25 +0300

    added .idea to ignored entries

---
+++ blog/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class StyleFormMixin:
+    def 
+
+class BlogForm(forms.ModelForm):
---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+
+from blog.apps import BlogConfig
+from blog.views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('create_blog/', BlogCreateView.as_view(), name='create'),
+    path('list_blog/', BlogListView.as_view(), name='list'),
+    path('view_blog/<int:pk>/', BlogDetailView.as_view(), name='view'),
+    path('view_blog/<slug:slug>/', BlogDetailView.as_view(), name='view'),
+    path('edit_blog/<int:pk>/', BlogUpdateView.as_view(), name='edit'),
+    path('delete_blog/<int:pk>/', BlogDeleteView.as_view(), name='delete'),
+    # path("__debug__/", include("debug_toolbar.urls")),
+]
